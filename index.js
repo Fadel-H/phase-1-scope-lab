34f358169b107f18e1e8c3fce55bdd30f4c364be
Fadel <fadel.g.heji@gmail.com>
2022-03-27 20:21:16 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+     bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(argument){
+    bestCustomer = argument
+}
+
+const leastFavoriteCustomer = "bob for sure"
+
+function changeLeastFavoriteCustomer(argument){
+    leastFavoriteCustomer = argument
+}
